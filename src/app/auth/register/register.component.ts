90e4c50cc7ef78316663e6eedfec3507ba026bde
omvir singh <omvir.singh.work@gmail.com>
2025-09-11 17:50:06 +0530

    fix angular issues

--- src/app/auth/register/register.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-register',
-  standalone: true,
-  template: `<p>Register works!</p>`
-})
-export class RegisterComponent {}
